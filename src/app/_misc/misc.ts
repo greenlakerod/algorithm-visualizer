61b97617bd6f4f9ecaba7bea91c0c9a36e4242f2
Rod Eligio <roderick.eligio@avanade.com>
2017-09-03 23:54:11 -0700

    fixed something

--- src/app/_misc/misc.ts
+++ src/app/_misc/misc.ts
@@ -15,6 +15,95 @@ function hasPairEqualToSum(data: Array<number>, sum: number) {
 
 //given a number, give the string representation of the number
 //e.g 100,715 -> "One Hundred Thousand Seven Hundred Fifteen"
+//"One Hundred 1 Thousand Seven Hundred undefinedteen"
+function toString(numString: string): string {
+    let value: string = "";
+    let digits = {
+        "0": "Zero",
+        "1": "One",
+        "2": "Two",
+        "3": "Three",
+        "4": "Four",
+        "5": "Five",
+        "6": "Six", 
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine"
+    };
+    let tens = {
+        "0": "Zero",
+        "1": "Ten",
+        "2": "Twenty",
+        "3": "Thirty",
+        "4": "Forty",
+        "5": "Fifty",
+        "6": "Sixty",
+        "7": "Seventy",
+        "8": "Eighty",
+        "9": "Ninety"
+    };
+    let powers = {
+        "1": "Thousand",  //1,000,000
+        "2": "Million",
+        "3": "Billion",
+        "4": "Trillion"
+    };
+
+    let chunks = numString.split(",");
+    for (let i = 0; i < chunks.length; i++) {
+        let chunk = chunks[i];
+        if ((chunk.length > 3) || (chunks.length > 1 && i > 1 && chunk.length < 3)) {
+            throw "Invalid number";
+        }
+
+        while (chunk.length < 3) {
+            chunk = "0" + chunk;
+        }
+        
+        let chunkAsString = "";
+        let hundred = digits[chunk[0]];
+        let ten = tens[chunk[1]];
+        let one = digits[chunk[2]];
+
+        if (hundred != "Zero") {
+            chunkAsString = hundred + " Hundred";
+        }
+        if (ten != "Zero") {
+            if (ten == "Ten") {
+                if (one != "Zero") {
+                    if (one == "One") {
+                        chunkAsString += " Eleven";
+                    } else if (one == "Two") {
+                        chunkAsString += " Twelve";
+                    } else if (one == "Three") {
+                        chunkAsString += " Thirteen";
+                    } else if (one == "Five") {
+                        chunkAsString += " Fifteen";
+                    } else {
+                        one += "teen";
+                        chunkAsString += " " + one;
+                    }
+                }
+            } else {
+                chunkAsString += " " + ten + " " + one;
+            }
+        } else {
+            if (one != "Zero") {
+                chunkAsString += " " + one;
+            } else if (hundred == "Zero") {
+                chunkAsString += " Zero";
+            }
+        }
+
+        if (i < chunks.length - 1) {
+            chunkAsString += " " + powers[chunks.length - 1 - i];
+        }
+
+        value += " " + chunkAsString;
+    }
+
+    return value.trim();
+}
 
 //given an array representation of a binary tree in inorder traversal, rebuild the binary tree
 
